378db6190366267ed253272a5fd90fd41097d62c
aneet2594 <aneet2594.be23@chitkara.edu.in>
2025-07-22 11:42:33 +0530

    Apple Pie

---
+++ Personality Quiz/Personality Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Aneet Kaur on 22/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
