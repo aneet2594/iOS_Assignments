84fd9dde03a59fb21fb4432cd84e6e752845faaa
aneet2594 <aneet2594.be23@chitkara.edu.in>
2025-08-25 13:33:10 +0530

    added files

--- TableViews/TableViews/EmojiTableViewController.swift
+++ TableViews/TableViews/EmojiTableViewController.swift
@@ -1,108 +1,108 @@
 //
-//  FoodTableViewController.swift
-//  Meal Tracker
+//  EmojiTableViewController.swift
+//  TableViews
 //
 //  Created by Aneet Kaur on 18/08/25.
 //
 
 import UIKit
 
-class FoodTableViewController: UITableViewController {
+class EmojiTableViewController: UITableViewController {
 
+    @IBSegueAction func addEditEmoji(_ coder: NSCoder, sender: Any?) -> AddEditTableViewController? {
+        guard let indexPath = sender as? IndexPath else {
+           return AddEditTableViewController(coder: coder, emoji: nil)
+        }
+        return AddEditTableViewController(coder: coder, emoji: emojis[indexPath.row])
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+    
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+         self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-    var meals: [Meal] {
-            let breakfast = Meal(name: "Breakfast", food: [
-                Food(emoji: "🥞", name: "Pancakes", description: "Fluffy pancakes with syrup"),
-                Food(emoji: "🍳", name: "Omelette", description: "Egg omelette with veggies"),
-                Food(emoji: "🍞", name: "Toast", description: "Whole grain toast with butter")
-            ])
-            
-            let lunch = Meal(name: "Lunch", food: [
-                Food(emoji: "🥗", name: "Salad", description: "Fresh green salad with dressing"),
-                Food(emoji: "🥪", name: "Sandwich", description: "Chicken sandwich with cheese"),
-                Food(emoji: "🍲", name: "Soup", description: "Tomato soup with croutons")
-            ])
-            
-            let dinner = Meal(name: "Dinner", food: [
-                Food(emoji: "🍝", name: "Pasta", description: "Pasta with marinara sauce"),
-                Food(emoji: "🍱", name: "Steak", description: "Grilled steak with mashed potatoes"),
-                Food(emoji: "🍣", name: "Fish", description: "Baked salmon with lemon")
-            ])
-            
-            return [breakfast, lunch, dinner]
-        }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return meals.count
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        
-        return meals[section].food.count
-        
+        return emojis.count
     }
 
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Food", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! EmojiTableViewCell
 
+        cell.updateUI(emoji: emojis[indexPath.row])
+        
+        cell.showsReorderControl = true
+        
         // Configure the cell...
 
-                
-            let meal = meals[indexPath.section]
-            let foodItem = meal.food[indexPath.row]
-        
-            var content = cell.defaultContentConfiguration()
-            content.text = foodItem.emoji + foodItem.name
-            content.secondaryText = foodItem.description
-            cell.contentConfiguration = content
-                
-            return cell
+        return cell
     }
-
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-            return meals[section].name
-        }
     
-    /*
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "editSegue", sender: indexPath)
+    }
+    
+
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            
+            emojis.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        
+        let removedEmoji = emojis.remove(at: fromIndexPath.row)
+        emojis.insert(removedEmoji, at: to.row)
+        
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    
+    @IBAction func unwindToEmojiTVC(segue: UIStoryboardSegue) {
+        guard segue.identifier == "saveSegue", let addEditTVC = segue.source as? AddEditTableViewController, let emoji = addEditTVC.emoji else { return }
+        guard let selectedIndexPath = tableView.indexPathForSelectedRow else {
+            emojis.append(emoji)
+            let indexPath = IndexPath(row: emojis.count - 1, section: 0)
+            tableView.insertRows(at: [indexPath], with: .fade)
+            return }
+        
+        emojis[selectedIndexPath.row] = emoji
+        tableView.reloadRows(at: [selectedIndexPath], with: .fade)
+        //tableView.reloadData()
+        
+    }
+    
 
     /*
     // Override to support conditional rearranging of the table view.
